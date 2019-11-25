12237074d84b373367e3fa173bc9373ffb53ff37
JKRuigu <ruigukjohn.com>
2019-11-25 03:37:19 +0300

    added treasure create validation

--- js/treasure.js
+++ js/treasure.js
@@ -1,6 +1,28 @@
 // CREATE TREASURE;
 createTreasure = (treasure=[])=>{
-		treasure = [{"x":createRandom(),"y":createRandom(),isFound:false}]; //create treasure;
+		let isTrue =true;
+		let dataLen = data.length;
+		console.log(data);
+		let blocksLen = blocks.length;
+		while(isTrue){
+			treasure = [{"x":createRandom(),"y":createRandom(),isFound:false}]; //create treasure;
+			let num =0;
+			for (var i = 0; i < dataLen; i++) {
+				if (data[i].x == treasure[0].x || data[i].y == treasure[0].y) {
+					num++;
+				}
+			}
+			for (var i = 0; i < blocksLen; i++) {
+				if (blocks[i].x == treasure[0].x || blocks[i].y == treasure[0].y) {
+					num++;
+				}
+			}
+			if (num == 0) {
+				isTrue = false;				
+			}else{
+				isTrue == true;
+			}
+		}
 		// console.log(treasure);
 		ctx.clearRect(treasure[0].x,treasure[0].y,sizeX,sizeY);
 		// ctx.drawImage(img, 100,580,100,400,treasure[0].x,treasure[0].y,sizeX,sizeY);
