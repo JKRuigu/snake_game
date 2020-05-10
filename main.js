4de89a46e3005dd70f63ac448a59dcf77b2f6c98
JKRuigu <ruigukjohn@gmail.com>
2020-05-10 15:16:57 +0300

    Added electron to the project

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({width: 850,height: 700});
+
+  // win.loadFile(url.format({
+  //   pathname:path.join(__dirname,'index2.html'),
+  //   protocol:'file',
+  //   slashes:true
+  // }));
+  win.loadFile('./index.html');
+  //devtools
+  win.webContents.openDevTools();
+
+  win.on('closed',()=>{
+    win = null;
+  })
+}
+
+app.on('ready',createWindow);
+
+//quit
+app.on('window-all-closed',() =>{
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
